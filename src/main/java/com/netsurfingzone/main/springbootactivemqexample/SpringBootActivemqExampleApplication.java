d22d1695fae3961f1c53db62788240ed216666f8
Naga Advik Reddy <dskkumardsk3@gmail.com>
2023-02-08 09:53:29 +0530

    initial commit

---
+++ src/main/java/com/netsurfingzone/main/springbootactivemqexample/SpringBootActivemqExampleApplication.java
@@ -0,0 +1,13 @@
+package com.netsurfingzone.main.springbootactivemqexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootActivemqExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootActivemqExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/netsurfingzone/main/springbootactivemqexample/SpringBootActivemqExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netsurfingzone.main.springbootactivemqexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootActivemqExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
